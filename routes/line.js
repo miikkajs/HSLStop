cd49e998e6ebbc60f890b429331b5383b3e36196
Miikka <m.salomaadev@gmail.com>
2014-01-22 18:40:17 +0200

    Changed res to send json

--- routes/line.js
+++ routes/line.js
@@ -4,9 +4,8 @@ var keysJSON = require('./keys.json');
 console.log(keysJSON.reittiopasApiKey);
 exports.lines = function(req, res) {
     request.get("http://api.reittiopas.fi/hsl/prod/?request=stop&code=1528&"+keysJSON.reittiopasApiKey, function(err, response, body) {
-        res.set('Content-Type', 'application/json');
         console.log(body);
-        res.send(body);
+        res.json(body);
 
     });
 
