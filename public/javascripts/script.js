fc9a02bdbc62bb480060050b330635028f638383
Miikka <m.salomaadev@gmail.com>
2014-03-06 18:01:02 +0200

    Show nearest stops

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,54 @@
 var app = angular.module('HSLStop', []);
-app.run(function($rootScope) {
- 
-});
+app.controller("FetchController", ["$scope" , "HSLService", function($scope, HSLService){
+    $scope.fetch = function(location){
+        $scope.data = [];
+        console.log("init");
+        HSLService.getStops().then(function(data){
 
-app.controller("FetchController", function($scope, $http){
-$scope.fetch = function(){
-  $http({method: 'GET', url: '/lines'}).
-  success(function(data, status, headers, config) {
- 
-    $scope.data = JSON.parse(data);
-       console.log(JSON.parse(data));
-      $scope.data = data;
-  }).
-  error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
- }
+                $scope.data = data.data;
+
+        });
+    }
+}]);
+app.service("HSLService", function($http, $q, $rootScope){
+    var service = {};
+
+    service.getStops = function(){
+        var deferred = $q.defer();
+        navigator.geolocation.getCurrentPosition(function(position){
+            $http({method: 'GET', url: '/lines?latitude='+position.coords.latitude+"&longitude="
+                + position.coords.longitude}).
+                success(function(data, status, headers, config) {
+                    var d1 =JSON.parse(data);
+                    deferred.resolve({data: JSON.parse(d1)});
+                    // update angular's scopes
+                    $rootScope.$$phase || $rootScope.$apply();
+
+                }).
+                error(function(data, status, headers, config) {
+                    deferred.reject();
+                    //return {error: data};
+                });
+        });
+        return deferred.promise;
+
+    }
+    return service;
+});
+app.service("LocationService", function($rootScope){
+    console.log("rootScope", $rootScope);
+    var myService = {};
+    myService.getLocation = function()
+    {
+        if (navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(myService.showPosition);
+        }
+
+    }
+    myService.showPosition = function(position)
+    {
+        myService.location = {latitude: position.coords.latitude, longitude : position.coords.longitude};
+    }
+    return myService;
 });
--- routes/line.js
+++ routes/line.js
@@ -1,11 +1,16 @@
 var request = require('request');
 var util = require('util');
 var keysJSON = require('./keys.json');
-console.log(keysJSON.reittiopasApiKey);
 exports.lines = function(req, res) {
-    request.get("http://api.reittiopas.fi/hsl/prod/?request=stop&code=1528&"+keysJSON.reittiopasApiKey, function(err, response, body) {
-        console.log(body);
-        res.json(body);
+    var latitude = req.query.latitude;
+    var longitude = req.query.longitude;
+    request.get("http://api.reittiopas.fi/hsl/prod/?request=stops_area&epsg_in=4326&epsg_out=4326&center_coordinate="+longitude+","+latitude+"&"+keysJSON.reittiopasApiKey, function(err, response, body) {
+        if(body){
+            res.json(body);
+        }else{
+            res.send(404);
+        }
+
 
     });
 
